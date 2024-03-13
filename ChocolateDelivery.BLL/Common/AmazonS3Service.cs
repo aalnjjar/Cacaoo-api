08668f35aad1ad7814a075baa470e6ef5be24a14
Islam Mahrous <isl.mahrous@gmail.com>
2024-03-13 18:53:23 +0300

    hardcoded CDN values

--- ChocolateDelivery.BLL/Common/AmazonS3Service.cs
+++ ChocolateDelivery.BLL/Common/AmazonS3Service.cs
@@ -10,8 +10,13 @@ public static class AmazonS3Service
 {
     private static AmazonS3Client _s3Client;
     private static string _bucket;
-    private static IConfiguration _config;
+    public const string CdnUrl = "https://chocopedia.s3.me-central-1.amazonaws.com/";
+    static AmazonS3Service()
+    {
+        _bucket = "chocopedia";
+        _s3Client = new AmazonS3Client("AKIA6GBMBSBQU5HLWEFR", "dz5Q6nAenYeYESF7HQ/0Q/kKV6o38OKRG5dei/oE", Amazon.RegionEndpoint.MECentral1);
 
+    }
     public static async Task<string> UploadToS3(IFormFile file, string subDirectory = "", string fileName = "")
     {
         var uniqueFileName = $"{fileName}_{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
@@ -42,13 +47,4 @@ public static class AmazonS3Service
 
         return response.HttpStatusCode == HttpStatusCode.OK;
     }
-    
-    public static void SetConfigs(IConfiguration config)
-    {
-        _config = config;
-        var accessKey = _config.GetSection("AmazonS3Credentials")["AccessKey"];
-        var accessSecret = _config.GetSection("AmazonS3Credentials")["AccessSecret"];
-        _bucket = _config.GetSection("AmazonS3Credentials")["Bucket"]!;
-        _s3Client = new AmazonS3Client(accessKey, accessSecret, Amazon.RegionEndpoint.MECentral1);
-    }
-}
+} 
